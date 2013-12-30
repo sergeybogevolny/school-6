d7e97f942c442c4ffba5f74ac037739ca4aaefa7
vishnu v <v36ishnu@yahoo.com>
2013-12-30 22:19:02 +0530

    home page design

---
+++ includes/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+define("APP_PATH", str_replace('includes', '', dirname ( __FILE__ )));
+define("APP_URL", "http://localhost/smu/");
+
+
---
+++ index.php
@@ -0,0 +1,526 @@
+<?php include 'includes/header.php'; ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Books online</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo APP_URL, 'css/basic.css'; ?>">
+	<script type="text/javascript" src="<?php echo APP_URL, 'js/jquery.js' ; ?>"></script>
+</head>
+<body>
+
+<div class="wrapper">
+	
+	<div class="header_wrapper">
+		<div class="title_box"><h1>Books Online</h1></div>
+		<div class="session_box">
+			<div class="cart" title="Items in shopping cart"><img class="cart_img" src="<?php echo APP_URL, 'img/cart_icon.png'; ?>" />2</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+
+
+
+<div class="content_wrapper">
+
+
+<div class="books_wrapper">
+
+	<div class="book_categorie_name"><h2>Academic and Professional</h2></div>
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+
+
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+<div class="clear"></div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="books_wrapper">
+
+	<div class="book_categorie_name"><h2>Biographies &amp; Autobiographies</h2></div>
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+
+
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+<div class="clear"></div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="books_wrapper">
+
+	<div class="book_categorie_name"><h2>Business, Investing and Management</h2></div>
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+
+
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+<div class="clear"></div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="books_wrapper">
+
+	<div class="book_categorie_name"><h2>Children</h2></div>
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+
+
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+	<div class="book">
+		<div class="book_img_wrapper">
+			<table class="image_tbl">
+				<tr>
+					<td valign="middle" align="center"><img src="<?php echo APP_URL, 'uploads/books/sample.jpeg';  ?>"></td>
+				</tr>
+			</table>
+		</div>
+		<div class="book_name">Compilers : Principles, Techniques, & Tools 2 Edition (Paperback)</div>
+		<div class="book_data">
+			Author: Ravi Sethi, D. Jeffrey Ulman, Alfred V. Aho, Monica S. Lam<br/>
+			Publisher: Dorling Kindersley (India) Pvt Ltd (2008)  
+		</div>
+		<span class="book_price">RS 250</span>
+		<span class="book_links"><a href="<?php echo APP_URL, 'add_to_cart.php?book_id=10'; ?>">Add to cart</a></span>
+		<div class="clear"></div>
+	</div>
+
+
+
+<div class="clear"></div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+	
+</div>
+	
+	<div class="footer_wrapper">
+		SMU BCA final project by jayasankar. Roll number: 511121985.
+	</div>
+
+</div>
+
+</body>
+</html>
+<?php include 'includes/footer.php'; ?>
